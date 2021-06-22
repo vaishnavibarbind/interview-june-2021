278f75c723971299e52e790c8e4f091fe90b5160
vaishnavibarbind <86282468+vaishnavibarbind@users.noreply.github.com>
2021-06-22 12:36:43 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -4,6 +4,19 @@ public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+   int[] bNum=new int[32];
+    int i=0;
+    String strnum="";
+    while(n>0){
+      bNum[i]=n%2;
+      n=n/2;
+      i++;
+    }
+    for(int j=i-1;j>=0;j--){
+      String xText = bNum[j] + "";
+      strnum=strnum+xtext;
+    }
+    return strnum;
+    
   }
 }
